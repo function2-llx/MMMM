074492a9b136591442c25fde1c79d1389b27cf28
function2 <function2-llx@outlook.com>
2024-02-05 21:06:41 +0800

    add patch processing script

--- scripts/data/patch.py
+++ scripts/data/patch.py
@@ -1,7 +1,7 @@
+import itertools as it
 from pathlib import Path
 
-import cytoolz
-import itertools as it
+import h5py
 import numpy as np
 import pandas as pd
 import torch
@@ -12,34 +12,43 @@ from luolib.utils import get_cuda_device, process_map
 
 from mmmm.data.defs import PROCESSED_SEG_DATA_ROOT
 
-mask_batch_size: int = 16
+mask_batch_size: int = 4
 patch_size: tuple3_t[int] = (96, 224, 224)
 
 def _load_mask(mask_dir: Path, name: str):
     return name, np.load(mask_dir / f'{name}.npy')
 
-def process_case(case_dir: Path, positive_classes: list[str]):
-    device = get_cuda_device()
-    masks = process_map(
-        _load_mask,
-        it.repeat(case_dir / 'masks'), positive_classes,
-        new_mapper=False, disable=True, max_workers=16,
-    )
-    masks = dict(masks)
-    for batch in cytoolz.partition_all(mask_batch_size, masks.items()):
-        # process_map(lambda name: np.load(case_dir / 'masks' / f'{name}.npy'), batch_classes, )
-        names, batch = zip(*batch)
-        batch = torch.as_tensor(np.concatenate(batch), device=device)
-        s = sliding_window_sum(batch, patch_size)
-        print(s.shape)
+def process_case(dataset_dir: Path, key: str):
+    case_dir = dataset_dir / 'data' / key
+    masks = torch.as_tensor(np.load(case_dir / 'masks.npy'), device=get_cuda_device())
+    patch_sum = []
+    dtype = torch.int32 if masks[0].numel() < torch.iinfo(torch.int32).max else torch.int64
+    mask_sizes = masks.new_empty((masks.shape[0], 1, 1, 1), dtype=dtype)
+    for i in range(0, masks.shape[0], mask_batch_size):
+        batch = masks[i:i + mask_batch_size]
+        mask_sizes[i:i + mask_batch_size] = batch.sum(dim=(1, 2, 3), keepdim=True, dtype=dtype)
+        patch_sum.append(sliding_window_sum(batch, patch_size, dtype))
+    patch_sum = torch.cat(patch_sum)
+    # patch contains 50% of the mask or 1000 mask voxels is positive
+    positive_th = (mask_sizes * 0.5).clamp(max=1000)
+    positive_mask = patch_sum > positive_th
+    save_dir = dataset_dir / 'patch' / ','.join(map(str, patch_size)) / f'.{key}'
+    save_dir.mkdir(exist_ok=True, parents=True)
+    np.save(save_dir / 'positive_mask.npy', positive_mask.cpu().numpy())
+    with h5py.File(save_dir / 'class_to_patch.h5', 'w') as f:
+        for i in range(masks.shape[0]):
+            positions = positive_mask[i].nonzero().short()
+            f.create_dataset(str(i), data=positions.cpu().numpy())
+    save_dir.rename(save_dir.with_name(key))
 
 def process_dataset(dataset_dir: Path):
+    save_root = dataset_dir / 'patch' / ','.join(map(str, patch_size))
     meta: pd.DataFrame = pd.read_pickle(dataset_dir / 'meta.pkl')
-    meta = meta.head(5)
+    keys = list(filter(lambda name: not (save_root / name).exists(), meta.index))
     process_map(
         process_case,
-        meta.index.map(lambda key: dataset_dir / 'data' / key), meta['positive_classes'],
-        ncols=80, max_workers=0, chunksize=1,
+        it.repeat(dataset_dir), keys,
+        ncols=80, max_workers=3, chunksize=1, total=len(keys),
     )
 
 def main():
