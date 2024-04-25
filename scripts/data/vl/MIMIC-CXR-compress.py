4f7a41dfd1e4c8a23fbfc0edcc0c7d373a148040
function2 <function2-llx@outlook.com>
2024-04-25 12:29:53 +0800

    refactor compression script; add compression for MIMIC-CXR

--- scripts/data/vl/MIMIC-CXR-compress.py
+++ scripts/data/vl/MIMIC-CXR-compress.py
@@ -1,31 +1,32 @@
 from copy import copy
 from pathlib import Path
 
+import einops
 import orjson
-import torch
+from torchvision.io import read_image
 
 from luolib.utils import get_cuda_device, process_map, save_pt_zst
-
 from _utils import crop_resize, check_cuda_cache
 
-RP_dir = Path('data/processed/vision-language/Radiopaedia')
-RP_image_dir = RP_dir / 'images'
-save_dir = Path('data/processed/vl-compressed/Radiopaedia')
-image_save_dir = save_dir / 'images'
+save_dir = Path('data/processed/vl-compressed/MIMIC-CXR')
 cuda_cache_th = 10
 
 def process_study(study: dict):
     try:
         study = copy(study)
         for i, image_path in enumerate(study['image']):
-            check_cuda_cache(cuda_cache_th)
+            check_cuda_cache()
             image_path = Path(image_path)
-            save_path = image_save_dir / image_path.relative_to(RP_image_dir).with_suffix('.pt.zst')
+            save_path = save_dir / image_path.relative_to('data/origin/vision-language/MIMIC-CXR-JPG').with_suffix('.pt.zst')
             study['image'][i] = str(save_path)
+            if study['modality'][i] == 'X-Ray':
+                study['modality'][i] = 'X-ray'
             if save_path.exists():
                 continue
             save_path.parent.mkdir(exist_ok=True, parents=True)
-            image = torch.load(image_path, map_location=get_cuda_device())
+            image = read_image(str(image_path))
+            image = image.to(device=get_cuda_device())
+            image = einops.rearrange(image, 'c h w -> c 1 h w')
             image = crop_resize(image)
             save_pt_zst(image.cpu(), save_path, atomic=True)
         return study
@@ -34,8 +35,8 @@ def process_study(study: dict):
         return None
 
 def process_split(split: str):
-    data_list = orjson.loads((RP_dir / f'{split}.json').read_bytes())
-    results = process_map(process_study, data_list, max_workers=0, chunksize=8, ncols=80)
+    data_list = orjson.loads((Path('data/processed/vision-language/MIMIC-CXR') / f'{split}.json').read_bytes())
+    results = process_map(process_study, data_list, max_workers=8, chunksize=8, ncols=80)
     Path(save_dir / f'{split}.json').write_bytes(orjson.dumps(results, option=orjson.OPT_INDENT_2))
 
 def main():
---
+++ scripts/data/vl/_utils.py
@@ -0,0 +1,35 @@
+import gc
+from itertools import starmap
+
+import math
+import torch
+from torchvision.transforms.v2.functional import to_dtype
+
+from luolib.utils import get_cuda_device
+from monai.transforms import generate_spatial_bounding_box
+from monai.utils import InterpolateMode
+import monai.transforms as mt
+
+def crop_resize(image: torch.Tensor):
+    crop_mask = (image > 0).all(0, keepdim=True)
+    start, end = generate_spatial_bounding_box(crop_mask)
+    image = image[:, *starmap(slice, zip(start, end))]
+    max_tokens_z = min(4, image.shape[1])
+    max_smaller_edge = int((256 / max_tokens_z) ** 0.5) * 32
+    resize_shape = [min(max_tokens_z * 32, image.shape[1]), *image.shape[2:]]
+    if (_base := min(resize_shape[1:])) > max_smaller_edge:
+        for j in (1, 2):
+            resize_shape[j] = math.ceil(resize_shape[j] * max_smaller_edge / _base)
+    if (resize_shape := tuple(resize_shape)) != image.shape[1:]:
+        image = to_dtype(image, scale=True)
+        resize = mt.Resize(resize_shape, mode=InterpolateMode.TRILINEAR, anti_aliasing=True)
+        image = resize(image)
+        image = to_dtype(image.as_tensor(), dtype=torch.uint8, scale=True)
+    return image
+
+def check_cuda_cache(cuda_cache_th: int = 10):
+    device = get_cuda_device()
+    cuda_cache = torch.cuda.memory_reserved(device) - torch.cuda.memory_allocated(device)
+    if cuda_cache > cuda_cache_th * 1024 ** 3:
+        gc.collect()
+        torch.cuda.empty_cache()
