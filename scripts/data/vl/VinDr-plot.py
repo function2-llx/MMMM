e553c4527771177b458ef34d14d6da17e2acde3a
function2 <function2-llx@outlook.com>
2024-04-26 11:56:11 +0800

    update VinDr-CXR plot

--- scripts/data/vl/VinDr-plot.py
+++ scripts/data/vl/VinDr-plot.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import torch
 
+from luolib.utils import process_map
 from monai.data import set_track_meta
 import monai.transforms as mt
 
@@ -13,17 +14,23 @@ data_dir = Path('data/origin/vision-language/VinDr-CXR')
 df = pd.read_csv(data_dir / f'annotations/annotations_{split}.csv')
 loader = mt.LoadImage(reader='itkreader')
 
+targets: dict[str, list] = {}
+for t in df.itertuples():
+    if t.class_name != 'No finding':
+        targets.setdefault(t.image_id, []).append(t)
+
 def plot(image_id: str):
     x: torch.Tensor = loader(data_dir / f'{split}/{image_id}.dicom')[..., 0]
     x = (x - x.min()) / (x.max() - x.min())
     x = x.T
-    case = df[df['image_id'] == image_id]
     dpi = 100
     fig, ax = plt.subplots(figsize=(x.shape[0] / dpi, x.shape[1] / dpi))
     ax: plt.Axes
     ax.imshow(x, cmap='gray')
     ax.set_axis_off()
-    for t in case.itertuples():
+    for t in targets[image_id]:
+        if t.class_name == 'No findings':
+            continue
         ax.add_patch(
             Rectangle(
                 (t.x_min, t.y_min), t.x_max - t.x_min, t.y_max - t.y_min,
@@ -34,13 +41,17 @@ def plot(image_id: str):
             (t.x_min + t.x_max) / 2, t.y_min - 15, t.class_name,
             color='white', fontsize=8, backgroundcolor='black', horizontalalignment='center',
         )
-    plt.show()
-    fig.savefig('vindr-plot.pdf', dpi=dpi, bbox_inches='tight', pad_inches=0)
-    fig.savefig('vindr-plot.png', dpi=dpi, bbox_inches='tight', pad_inches=0)
+    save_dir = Path(f'VinDr-CXR-plot/{image_id}')
+    save_dir.mkdir(exist_ok=True, parents=True)
+    fig.savefig(save_dir / 'plot.pdf', dpi=dpi, bbox_inches='tight', pad_inches=0)
+    fig.savefig(save_dir / 'plot.png', dpi=dpi, bbox_inches='tight', pad_inches=0)
 
 def main():
     set_track_meta(False)
-    plot('01ded16689539deb30d0981fafd18465')
+    process_map(
+        plot, list(targets.keys()),
+        max_workers=0, chunksize=8, ncols=80,
+    )
 
 if __name__ == '__main__':
     main()
