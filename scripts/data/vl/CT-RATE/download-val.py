eed20c7e203440a1dc14917d23e805d29b315679
function2 <function2-llx@outlook.com>
2024-05-03 01:16:00 +0800

    CT-RATE download: add try-except

--- scripts/data/vl/CT-RATE/download-val.py
+++ scripts/data/vl/CT-RATE/download-val.py
@@ -3,15 +3,20 @@ from huggingface_hub import snapshot_download
 from mmmm.data.defs import ORIGIN_DATA_ROOT, ORIGIN_VL_DATA_ROOT
 
 def main():
-    snapshot_download(
-        'ibrahimhamamci/CT-RATE',
-        repo_type='dataset',
-        allow_patterns='dataset/valid/**',
-        resume_download=True,
-        cache_dir=ORIGIN_DATA_ROOT / '.hub',
-        local_dir=ORIGIN_VL_DATA_ROOT / 'CT-RATE',
-        local_dir_use_symlinks=True,
-    )
+    while True:
+        try:
+            snapshot_download(
+                'ibrahimhamamci/CT-RATE',
+                repo_type='dataset',
+                allow_patterns='dataset/valid/**',
+                resume_download=True,
+                cache_dir=ORIGIN_DATA_ROOT / '.hub',
+                local_dir=ORIGIN_VL_DATA_ROOT / 'CT-RATE',
+                local_dir_use_symlinks=True,
+            )
+            break
+        except:
+            pass
 
 if __name__ == '__main__':
     main()
