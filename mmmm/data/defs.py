594e4369e7045fe37ada9a48c9a5a7c414e5f879
function2 <function2-llx@outlook.com>
2024-05-03 17:59:31 +0800

    update data point definition

--- mmmm/data/defs.py
+++ mmmm/data/defs.py
@@ -1,12 +1,8 @@
 from __future__ import annotations as _
 
-from dataclasses import field
-from functools import partial
 from pathlib import Path
 from typing import Literal, NamedTuple, TypedDict
 
-from mashumaro import pass_through
-import numpy as np
 import torch
 
 from luolib.types import tuple3_t
@@ -20,36 +16,37 @@ PROCESSED_SEG_DATA_ROOT = PROCESSED_DATA_ROOT / 'image'
 PROCESSED_VL_DATA_ROOT = PROCESSED_DATA_ROOT / 'vision-language'
 PROCESSED_VG_DATA_ROOT = PROCESSED_DATA_ROOT / 'visual-grounding'
 
-def _numpy_field(dtype: np.dtype):
-    return field(metadata={'serialize': pass_through, 'deserialize': partial(np.array, dtype=dtype)})
-
 class DataPoint(TypedDict):
     """
     Attributes:
-        mask: (c, *spatial)
-        mask_index: select targets from text that corresponds to the mask
-        bbox: (c, [center, size]), or (c, 2, 3)
+        mask_idxes: for each target, indicating the corresponding mask index of each instance
+            if , no mask label for this target
+        num_uncertain: number of uncertain instances for the target.
+            -1 to ignore this target when calculating grounding loss
+        semantic: the mask label for this class is semantic
     """
     image: torch.Tensor
-    grounding_image: torch.Tensor | None
+    grounding_image: torch.Tensor
     patch_size: tuple3_t[int]
     pool_size: tuple3_t[int]
     vlm_inputs: dict[str, torch.Tensor]
-    mask: torch.BoolTensor
-    mask_index: torch.BoolTensor
-    bbox: torch.Tensor
-    bbox_index: torch.BoolTensor
+    masks: torch.BoolTensor
+    mask_idxes: list[torch.LongTensor]
+    boxes: list[torch.Tensor]
+    num_uncertain: torch.LongTensor
+    semantic: torch.BoolTensor
 
 class Batch(TypedDict):
     image: list[torch.Tensor]
-    grounding_image: list[torch.Tensor | None]
+    grounding_image: list[torch.Tensor]
     patch_size: list[tuple3_t[int]]
     pool_size: list[tuple3_t[int]]
     vlm_inputs: dict[str, torch.Tensor]
-    mask: list[torch.BoolTensor]
-    mask_index: list[torch.BoolTensor]
-    bbox: list[torch.Tensor]
-    bbox_index: list[torch.BoolTensor]
+    masks: list[torch.BoolTensor]
+    mask_idxes: list[torch.LongTensor]
+    boxes: list[torch.Tensor]
+    num_uncertain: list[torch.LongTensor]
+    semantic: list[torch.BoolTensor]
 
 split_t = Literal['train', 'val']
 CE_IGNORE_INDEX = -100
