d8f1f1aaeec7426ffc4d11cfe10196cd249e9d77
function2 <function2-llx@outlook.com>
2024-05-11 20:25:21 +0800

    fix load lora

--- scripts/phase-1/cli.py
+++ scripts/phase-1/cli.py
@@ -81,7 +81,7 @@ class CLI(LightningCLI):
         lora_config.target_modules, lora_config.modules_to_save = get_lora_modules_default(model)
         peft_model = get_peft_model(model, lora_config)
         model.set_peft_model(peft_model)
-        if (lora_adapter_path := config.lora_adapter_path) is None:
+        if (lora_adapter_path := config.lora_adapter_path) is not None:
             peft_model.load_adapter(str(lora_adapter_path), 'default', is_trainable=self.subcommand == 'fit')
             print(f'load adapter from {lora_adapter_path}')
 
