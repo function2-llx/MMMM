12363af4cf6000b8f9bf200a44bf18933cfbdf3b
function2 <function2-llx@outlook.com>
2024-05-17 00:25:55 +0800

    fix debug output

--- mmmm/models/mmmm.py
+++ mmmm/models/mmmm.py
@@ -148,6 +148,9 @@ class MMMMForCausalLM(CogVLMForCausalLM, LightningModule):
         self.model.tokenizer = tokenizer
         self.check_grad = False
         self._setup_freeze(freeze_vg)
+        # DEBUG
+        self.model.vision.requires_grad_(False)
+        self.model.layers.requires_grad_(False)
         self.disable_vg = disable_vg
         return self
 
@@ -259,9 +262,6 @@ class MMMMForCausalLM(CogVLMForCausalLM, LightningModule):
         # TODO: replace with checkpoint wrapper
         # https://github.com/pytorch/pytorch/blob/main/torch/distributed/algorithms/_checkpoint/checkpoint_wrapper.py
         self.gradient_checkpointing_enable({'use_reentrant': False})
-        # DEBUG
-        self.model.vision.requires_grad_(False)
-        self.model.layers.requires_grad_(False)
         # NOTE: there may be some code setting lora.Linear.base_layer.eval(),
         #  however, let's keep it "training" to make DeepSpeed work, since it is just a linear layer
         #  and is not affected by the mode
