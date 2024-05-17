8956808e3fdc24bef639b1149d128b74869d596c
function2 <function2-llx@outlook.com>
2024-05-18 03:20:53 +0800

    adjust VQA-Med

--- scripts/data/vl/VQA-Med.py
+++ scripts/data/vl/VQA-Med.py
@@ -1,6 +1,7 @@
 import json
 import os
 import shutil
+
 from tqdm import tqdm
 
 from mmmm.data.defs import ORIGIN_VL_DATA_ROOT, PROCESSED_VL_DATA_ROOT
@@ -37,10 +38,10 @@ def process_text(txt_file: str, out_file: str, test: bool =False):
 
 def process_images(img_dir: str, ):
     os.makedirs(PROCESSED_VL_DATA_ROOT / 'VQA-Med' / 'images', exist_ok=True)
-    for img in tqdm(os.listdir(ORIGIN_VL_DATA_ROOT / 'VQA-Med' / img_dir)):
+    for img_path in tqdm(list((ORIGIN_VL_DATA_ROOT / 'VQA-Med' / img_dir).iterdir())):
         shutil.copy(
-            ORIGIN_VL_DATA_ROOT / 'VQA-Med' / img_dir / img,
-            PROCESSED_VL_DATA_ROOT / 'VQA-Med' / 'images' / img
+            img_path,
+            PROCESSED_VL_DATA_ROOT / f'VQA-Med/images/{img_path.name}',
         )
 
 def process():
