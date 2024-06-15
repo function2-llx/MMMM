ec772d650d9497f2ab273cff987bb2de029a76fc
function2 <function2-llx@outlook.com>
2024-06-15 18:52:35 +0800

    let go party vindr

--- scripts/align-sam/cli.py
+++ scripts/align-sam/cli.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+import torch
+
 from luolib.lightning.cli import LightningCLI
 
 from _data import DataModule
@@ -6,6 +9,17 @@ from _model import AlignSam
 class CLI(LightningCLI):
     model: AlignSam
     datamodule: DataModule
+
+    def add_arguments_to_parser(self, parser: 'LightningArgumentParser'):
+        super().add_arguments_to_parser(parser)
+        parser.add_argument('--state_dict_path', type=Path | None, default=None)
+
+    def instantiate_classes(self) -> None:
+        super().instantiate_classes()
+        if state_dict_path := self.active_config_init.state_dict_path:
+            ckpt = torch.load(state_dict_path)
+            self.model.load_state_dict(ckpt['state_dict'])
+            print(f'load state dict from checkpoint: {self.active_config_init.state_dict_path}')
 
 def main():
     CLI(
